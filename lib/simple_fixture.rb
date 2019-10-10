593df9e41bcd48554ecf98e4ca2e6034fba368e8
ken <block24block@gmail.com>
2019-10-10 14:29:00 +0800

    create db in ./tmp and solve symbol and string problem amongs activerecord versions

--- lib/simple_fixture.rb
+++ lib/simple_fixture.rb
@@ -3,15 +3,13 @@ require "active_record"
 
 module SimpleFixture
 
+  DB_DIR = 'tmp'
   DB_NAME = 'simple_fixture'
 
   class << self
-    def create(name: DB_NAME, renew: false)
-      path = "/tmp/#{name}.splite3"
-      `rm #{path}` if File.exists? path
-
-      ::ActiveRecord::Base.configurations = {test: {adapter: 'sqlite3', database: path}}
-      ::ActiveRecord::Base.establish_connection(:test)
+    def create(name: DB_NAME)
+      path = build_db_file(name)
+      establish_connection(path)
 
       load 'test/simple_fixture/migration.rb'
     end
@@ -22,6 +20,22 @@ module SimpleFixture
           instance_eval &block
         end
       end.new.change
+    end
+
+    private
+
+    def build_db_file(name)
+      Dir.mkdir DB_DIR unless Dir.exists? DB_DIR
+      path = File.join(DB_DIR, "#{name}.splite3")
+      File.delete(path) if File.exists? path
+      path
+    end
+
+    def establish_connection(path)
+      ::ActiveRecord::Base.configurations = {test: {adapter: 'sqlite3', database: path}}.with_indifferent_access
+      ::ActiveRecord::Base.establish_connection(:test)
+    rescue => _
+      ::ActiveRecord::Base.establish_connection('test')
     end
   end
 end
